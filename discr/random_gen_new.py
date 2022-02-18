4152a58260a102fc76490c1153ee0a2636ec80a3
Nanni Bassetti <nannib@users.noreply.github.com>
2022-02-18 17:31:52 +0100

    Add files via upload

--- discr/random_gen_new.py
+++ discr/random_gen_new.py
@@ -1,34 +1,36 @@
 import random
-import csv
+
 i=0
 
-while i<3000:
+header = "age,"+"breed,"+"past_crimes,"+"suspect"
+print(header)
+db = open("db.csv", "w")
+print(header,file=db)
+
+while i<200:
       i=i+1
       age=random.randint(10, 60)
       age2=random.randint(10, 60)
       breed=random.randint(0,1)
-      #if b==2:
-        #  b=1
+      if breed==0:
+          strbreed="Martian"
+      else:
+          strbreed="Terrestrial"
       crimes=random.randint(0,10)
       crimes2=random.randint(0,10)
       rand_badterrestrial=random.randint(1,10)
-      if (rand_badterrestrial==2 or rand_badterrestrial==5 or rand_badterrestrial==7 or rand_badterrestrial==9) or (crimes>crimes2 and breed==2) or (age<age2 and breed==2):
+      if (rand_badterrestrial==2 or rand_badterrestrial==5 or rand_badterrestrial==7 or rand_badterrestrial==9) or (crimes>crimes2 and breed==0) or (age<age2 and breed==0):
           label="yes"
       else:
           label="no"
-      print(age,",",breed,",",crimes,",",label)
+      print(age,",",strbreed,",",crimes,",",label)
         
 
-      header = ['age', 'breed', 'past_crimes', label]
-      data = [age,breed,crimes,label]
+      
+      data = str(age)+","+strbreed+","+str(crimes)+","+label
 
-      with open('disct01.csv', 'a', encoding='UTF8', newline='') as f:
-          writer = csv.writer(f)
+      print(data,file=db)
 
-        # write the header
-          #writer.writerow(header)
-
-        # write the data
-          writer.writerow(data)
+db.close()
 random.seed()
 
